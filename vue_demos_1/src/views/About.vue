428c3f3b9c96a6ab6ed42ae44ffb4fddcd594f42
teachat <teachat@aliyun.com>
2019-06-18 23:20:45 +0800

    详解vue-router

---
+++ vue_demos_1/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <router-link to="/">返回Home页面</router-link>
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  mounted() {
+    console.log(111)
+    console.log('方式1.path方式接受参数', this.$route.query.code)
+    console.log('方式2', this.$route.params.code)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_demos_1/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <p>方式1.path</p>
+    <button @click="goToAbout">path利用js事件传递路由参数</button>
+    <br/>
+    <!-- path与query方式1.使用router-link路由跳转传参 -->
+    <router-link :to="{path: '/about', query: {code: '1'}}">path利用router-link传递路由参数</router-link>
+    <br/>
+    <p>方式2.name</p>
+    <button @click="namegoToAbout">name利用js事件传递路由参数</button>
+    <br/>
+    <!-- name与params方式1.使用router-link路由跳转传参 -->
+    <router-link :to="{name: 'about', params: {code: '2'}}">name利用router-link传递路由参数</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    // path与query方式2.js事件路由跳转传参
+    goToAbout() {
+      this.$router.push({
+        path: '/about',
+        query: {
+          code: '1'
+        }
+      })
+    },
+    // name与params方式2.js事件路由跳转传参
+    namegoToAbout() {
+      this.$router.push({
+        name: 'about',
+        params: {
+          code: '2'
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
