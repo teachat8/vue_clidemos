c603f29c8a1d47b8d9b970f137a69433b06a1239
teachat <teachat@aliyun.com>
2019-06-18 23:58:23 +0800

    详解父子传值，兄弟组件传值bus

---
+++ vue_demos_2/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>This is Hello</div>
+</template>
+
+<script>
+import Bus from '../bus'
+export default {
+  name: 'hello',
+  mounted() {
+    Bus.$on('msg', (e) => {
+      console.log(e)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_demos_2/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="welcome to your vuejs" @setMsg="getChildMsg"/>
+    <Hello/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+import Hello from '../components/Hello'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Hello
+  },
+  methods: {
+    getChildMsg(childMsg) {
+      console.log('子组件传递过来的数据为--->>>', childMsg)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
